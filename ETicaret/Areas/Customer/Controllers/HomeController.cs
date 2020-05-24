a39ebc0af175d29d6c56d21a7ee8e9dc00b1e5bc
Freelancer <63797314+baharerkanmutlu@users.noreply.github.com>
2020-05-25 00:12:14 +0300

    Create Areas Sections(Admin And Customer)

--- ETicaret/Areas/Customer/Controllers/HomeController.cs
+++ ETicaret/Areas/Customer/Controllers/HomeController.cs
@@ -7,16 +7,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ETicaret.Models;
 
-namespace ETicaret.Controllers
+namespace ETicaret.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
---
+++ MainETicaret.Utility/ProjectConstant.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainETicaret.Utility
+{
+    class ProjectConstant
+    {
+    }
+}
